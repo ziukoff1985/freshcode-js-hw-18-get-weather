c28094a996ded0c1cde0be9986b38e89cbac8489
Borys Ziukov <ziukoff1985@gmail.com>
2025-10-24 18:30:08 +0300

    Fix styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const radio = document.querySelectorAll('.radio');
+const input = document.querySelectorAll('.input');
+
+const [cityName, cityId] = input;
+const [cityNameRadio, cityIdRadio] = radio;
+
+cityName.focus();
+cityId.disabled = true;
+cityName.disabled = false;
+cityId.classList.add('disabled');
+cityName.classList.remove('disabled');
+
+cityNameRadio.addEventListener('change', () => {
+    cityId.disabled = true;
+    cityName.disabled = false;
+    cityId.classList.add('disabled');
+    cityName.classList.remove('disabled');
+    cityName.focus();
+    cityId.value = '';
+});
+
+cityIdRadio.addEventListener('change', () => {
+    cityName.disabled = true;
+    cityId.disabled = false;
+    cityName.classList.add('disabled');
+    cityId.classList.remove('disabled');
+    cityId.focus();
+    cityName.value = '';
+});
