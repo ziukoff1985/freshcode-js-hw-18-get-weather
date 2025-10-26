a06e5bedee9fbf33aff82d3b0389c0919f7fcce0
Borys Ziukov <ziukoff1985@gmail.com>
2025-10-26 13:45:15 +0200

    Finally after optimazation

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const API_KEY = '7dadff2236bcd1bfd68b5922550ee28c';
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
+
+export { BASE_URL };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 'use strict';
+import { BASE_URL } from './constants.js';
 
-const API_KEY = '7dadff2236bcd1bfd68b5922550ee28c';
-const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
-
-const radios = document.querySelectorAll('.radio');
 const inputs = document.querySelectorAll('.input');
-const buttons = document.querySelectorAll('.button');
+const radios = document.querySelectorAll('.radio');
+const weatherProps = document.querySelectorAll('.prop-value');
+const cancelBtn = document.querySelector('button[type="reset"]');
 const form = document.querySelector('.form');
 
 const [cityNameRadio, cityIdRadio] = radios;
 const [cityNameInput, cityIdInput] = inputs;
-const [getWeatherBtn, cancelBtn] = buttons;
 
 function setInitialState() {
     cityNameRadio.checked = true;
@@ -25,26 +23,22 @@ radios.forEach((radio) => {
 });
 
 function toggleDisabledFields() {
-    const weatherProps = document.querySelectorAll('.prop-value');
-    weatherProps.forEach((element) => (element.textContent = '- - -'));
-    inputs.forEach((input) => (input.value = ''));
-    cityNameRadio.checked ? searchByCityName() : searchByCityId();
+    weatherProps.forEach((element) => {
+        element.textContent = '- - -';
+        element.classList.remove('error');
+    });
+    cityNameRadio.checked
+        ? toggleInputs(cityNameInput, cityIdInput)
+        : toggleInputs(cityIdInput, cityNameInput);
 }
 
-function searchByCityName() {
-    cityIdInput.disabled = true;
-    cityNameInput.disabled = false;
-    cityIdInput.classList.add('disabled');
-    cityNameInput.classList.remove('disabled');
-    cityNameInput.focus();
-}
-
-function searchByCityId() {
-    cityNameInput.disabled = true;
-    cityIdInput.disabled = false;
-    cityNameInput.classList.add('disabled');
-    cityIdInput.classList.remove('disabled');
-    cityIdInput.focus();
+function toggleInputs(activeField, inactiveField) {
+    activeField.disabled = false;
+    inactiveField.disabled = true;
+    activeField.classList.remove('disabled');
+    inactiveField.classList.add('disabled');
+    inactiveField.value = '';
+    activeField.focus();
 }
 
 async function getWeatherData(city) {
@@ -52,25 +46,21 @@ async function getWeatherData(city) {
         const url = cityNameRadio.checked
             ? `${BASE_URL}&q=${city}`
             : `${BASE_URL}&id=${city}`;
-
         const response = await fetch(url);
         const data = await response.json();
-
-        console.log(data);
-
         if (data.cod !== 200) throw new Error(data.message);
-
         renderWeatherData(data);
     } catch (error) {
         console.log(error);
+        renderError(error.message);
     }
 }
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const city = cityNameRadio.checked
-        ? cityNameInput.value
-        : cityIdInput.value;
+        ? cityNameInput.value.trim()
+        : cityIdInput.value.trim();
     getWeatherData(city);
 });
 
@@ -81,9 +71,15 @@ function renderWeatherData(data) {
         main: { temp, humidity },
         wind: { speed },
     } = data;
-    const weatherProps = document.querySelectorAll('.prop-value');
     const [tempElement, windElement, humidityElement] = weatherProps;
     tempElement.textContent = `${temp} °C`;
     humidityElement.textContent = `${humidity} %`;
     windElement.textContent = `${speed} m/s`;
+}
+
+function renderError(message) {
+    weatherProps.forEach((element) => {
+        element.textContent = message;
+        element.classList.add('error');
+    });
 }
